6264eed13e7d237878352d3d1ac36d32cc31b86c
Sean Teare <steare@redventures.com>
2015-05-20 00:43:57 -0500

    add routes for setting ball color and rolling params

--- LumenProject/app/Http/routes.php
+++ LumenProject/app/Http/routes.php
@@ -14,3 +14,69 @@
 $app->get('/', function() use ($app) {
     return $app->welcome();
 });
+
+$app->get('/nextCommand', function () use ($app) {
+    $redis = new Predis\Client();
+    return $redis->rpop('spheroCommands');
+});
+
+$app->get('/roll/ball/{ball}/angle/{angle}/speed/{speed}', function ($ball, $angle, $speed) {
+    $validBalls = array ('0', '1', '2');
+
+    if (!in_array($ball, $validBalls)) {
+        return 'Invalid ballId';
+    }
+
+    if ($angle > 359 || $angle < 0) {
+        return 'Invalid angle...should be between 0 and 359';
+    }
+
+    if ($speed > 1000 || $speed < 10) {
+        return 'Invalid speed...shoudl be between 10 and 1000';
+    }
+    $data = array(
+        'action' => 'roll',
+        'angle' => $angle,
+        'speed' => $speed,
+        'ball' => $ball
+    );
+
+    $redis = new Predis\Client(array('host' => 'personal-rds.cdgfbmp7jqyx.us-west-2.rds.amazonaws.com', 'port' => 6379));
+    $redis->lpush('spheroCommands', json_encode($data));
+    return 'Success';
+});
+
+$app->get('/color/ball/{ball}/red/{red}/green/{green}/blue/{blue}', function ($ball, $red, $green, $blue) {
+
+    $validBalls = array ('0', '1', '2');
+
+    if (!in_array($ball, $validBalls)) {
+        return 'Invalid ballId';
+    }
+
+    $min = 0;
+    $max = 255;
+
+    if ($red < $min || $red > $max) {
+        return 'Invalid red value...should be between 0 and 255';
+    }
+    if ($green < $min || $green > $max) {
+        return 'Invalid green value...should be between 0 and 255';
+    }
+
+    if ($blue < $min || $blue > $max) {
+        return 'Invalid blue value...should be between 0 and 255';
+    }
+
+    $data = array(
+        'action' => 'color',
+        'red' => $red,
+        'green' => $green,
+        'blue' => $blue
+    );
+
+     $redis = new Predis\Client(array('host' => 'personal-rds.cdgfbmp7jqyx.us-west-2.rds.amazonaws.com', 'port' => 6379));
+    $redis->lpush('spheroCommands', json_encode($data));
+    return 'Success';
+
+});
